b3a82f0018d2a759f08309e73b6c7d5e13f996ea
mwomack <mwomack@pcoe.k12.ca.us>
2020-11-25 15:58:53 -0800

    1st commit

---
+++ linear_Fractal.js
@@ -0,0 +1,101 @@
+let shr=(3+Math.sqrt(5))/2;
+function pentL(t, len,lev){
+if(lev==0){t.fd(len);}
+else{
+t.lt(36);pentL(t,len/shr,lev-1);
+t.lt(72);pentL(t,len/shr,lev-1);
+t.rt(144);pentL(t,len/shr,lev-1);
+t.rt(72);pentL(t,len/shr,lev-1);
+t.lt(72);pentL(t,len/shr,lev-1);
+t.lt(72);pentL(t,len/shr,lev-1);
+t.rt(36);
+}
+}
+let srt=Math.sqrt(2);
+
+function bLeaf(t,len,lev){
+	if(lev<1){t.fd(2*len);t.bk(2*len);}
+	else{t.fd(len);
+	bLeaf(t,len/2,lev-2);
+	t.bk(len);t.lt(45);
+	bLeaf(t,len/srt,lev-1);
+	t.rt(90);
+	bLeaf(t,len/srt,lev-1);
+t.lt(45);}
+}
+
+function sd(t,len,lev,p){
+	if(lev==0){t.fd(len)}
+	else{
+		t.lt(60*p);sd(t,len/2,lev-1,p*(-1));
+        t.rt(60*p);sd(t,len/2,lev-1,p);
+        t.rt(60*p);sd(t,len/2,lev-1,p*(-1));
+        t.lt(60*p);
+		}
+}
+
+function Ngon(t, len,lev){
+	if(lev==0){sd(t,50,3,1);}//t.fd(len);}
+	else{
+	Ngon(t,len/3,lev-1);t.lt(90);	
+	Ngon(t,len/3,lev-1);t.rt(90);
+	Ngon(t,len/3,lev-1);t.rt(90);
+	Ngon(t,len/3,lev-1);t.rt(90);
+	Ngon(t,len/3,lev-1);t.lt(90);
+	Ngon(t,len/3,lev-1);t.lt(90);
+	Ngon(t,len/3,lev-1);t.lt(90);
+	Ngon(t,len/3,lev-1);t.rt(90);
+	Ngon(t,len/3,lev-1);
+	}
+}
+
+function hilbert(t,len,lev,p){
+	if(lev==0){}
+	else{
+		t.lt(p*90);hilbert(t,len,lev-1,-1*p);t.fd(len);
+		t.rt(p*90);hilbert(t,len,lev-1,p);t.fd(len);
+		hilbert(t,len,lev-1,p);t.rt(p*90);
+		t.fd(len);hilbert(t,len,lev-1,-1*p);t.lt(p*90);
+	}
+}
+
+function siepTri(t,len,lev){
+	if(lev==0){}
+	else{
+		siepTri(t,len/2,lev-1);t.fd(len);t.rt(120);
+		siepTri(t,len/2,lev-1);t.fd(len);t.rt(120);
+		siepTri(t,len/2,lev-1);t.fd(len);t.rt(120);
+		}
+}
+
+function C(t,len,lev){
+	if(lev==0){t.fd(len);}
+	else{
+	C(t,len,lev-1);t.lt(90);
+    C(t,len,lev-1);t.rt(90);	
+	}
+}
+
+function test1(){
+	clear_Can();
+	let num=document.getElementById("num1").value;
+	let numb=Math.floor(num);
+	switch(numb){
+		case 1:
+		tS.set1(100,400,0);pentL(tS,500,4);break;
+		case 2:tS.set1(400,400,-90);bLeaf(tS,150,8);break;
+		case 3:tS.set1(200,470,0);sd(tS,500,7,1);break;
+		case 4:tS.set1(400,500,-90);Ngon(tS,200,2);break;
+		case 5:tS.set1(700,470,-90);hilbert(tS,10,6,1);break;
+		case 6:tS.set1(200,470,-60);siepTri(tS,500,6);break;
+		case 7:tS.set1(500,400,0);C(tS,6,11);break;
+	}
+	
+}
+//pentL(tS,600,5);
+//bLeaf(tS,100,9);
+//sd(tS,200,3,1);
+//Ngon(tS,250,2);
+//hilbert(tS,10,6,1)6;
+//siepTri(tS,200,5);
+//C(tS,3,13);
---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+function start(){
+ 
+ tS.c.lineWidth=1;
+tS.c.strokeStyle="blue";
+//sq();
+tS.set1(400,150,-45);
+//pentL(tS,600,5);
+//bLeaf(tS,100,9);
+//sd(tS,200,3,1);
+//Ngon(tS,250,2);
+//hilbert(tS,10,6,1)6;
+//siepTri(tS,200,5);
+//C(tS,3,13);
+}
+
+function clear_Can(){
+	 let canA=document.getElementById("can1");
+	 let w=canA.width;
+	 canA.width=w;//clears the screen.
+ }
